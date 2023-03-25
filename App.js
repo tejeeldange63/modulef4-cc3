dc88bc633de14f88f18cfa536721fe83c3fd5517
Tejeel sunil dange <119481521+tejeeldange63@users.noreply.github.com>
2023-03-25 22:27:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import PincodeLookup from "./componets/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <PincodeLookup />
+      
+    </div>
+  );
+}
+
+export default App;
